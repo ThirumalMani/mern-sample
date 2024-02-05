ae593347a8d0df5107c69c0f0b18f2dfd7b9db19
thirumalm <info.thirumal@gmail.com>
2024-02-05 15:23:52 +0530

    basecode

--- flipkart_clone-main/serves/models/token.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const tokenSchema = mongoose.Schema({
-    token:String,
-})
-
-const Token = new mongoose.model("token",tokenSchema);
-
-module.exports = Token
